6eabdb2629de0388a6709e234f7efb17a992078b
bobbyy16 <bobbyyyyy16@gmail.com>
2023-07-06 17:59:08 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar'
+import './index.css'
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar">
+       <img src='/' alt="There supposed to be image here" />
+
+      </nav>      
+    </div>
+  )
+}
