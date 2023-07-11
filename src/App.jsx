7aff45cbfdb537ef67f1e6c265297e63b565f441
bobbyy16 <bobbyyyyy16@gmail.com>
2023-07-11 20:42:56 +0530

    modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 import './index.css'
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Navbar/>
+      <Footer/>
     </>
   )
 }
