70db593bf97f8adf7c05e859bb99b177df090b92
bobbyy16 <bobbyyyyy16@gmail.com>
2023-07-11 20:08:27 +0530

    Added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,14 @@ export default function Navbar() {
   return (
     <div>
       <nav className="navbar">
-       <img src='/' alt="There supposed to be image here" />
-
+      <a href='https://www.linkpicture.com/view.php?img=LPic64ad661c1b6861708777206'>
+        <img src='https://www.linkpicture.com/q/HL.png' className='logo' alt='image' />
+      </a>      
+      <div className="links">
+        <a href="/">Home</a>
+        <a href="/companies">Companies</a>
+        <a href="/open-source">Open-source</a>
+      </div>
       </nav>      
     </div>
   )
