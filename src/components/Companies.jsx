9b4b9c0aece5fe308c780aed479658cd4acb217d
bobbyy16 <bobbyyyyy16@gmail.com>
2023-07-16 11:12:52 +0530

    card added for both open-source and companies

--- src/components/Companies.jsx
+++ src/components/Companies.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import companyData from '../DB/company.json'
+
 
 function Companies() {
+  const filteredData = companyData.filter(company => company.category === 'companies');
   return (
     <div>
       <Navbar/>
-      Comapnies
+      <div className="card-container">
+        {filteredData.map(company => (
+          <div className="card" key={company.company_name}>
+            <img src={company.image} alt={company.company_name} />
+            <h3>{company.company_name}</h3>
+            <p>{company.description}</p>
+            <a href={company.link}>Visit Website</a>
+            <a href={company.carrers}>Careers</a>
+          </div>
+        ))}
+      </div>
       <Footer/>
     </div>
   )
