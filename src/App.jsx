03fb87e51fd07de35f26a8a5cd29b5a40b3f3982
Abhishek k <bobbyyyyy.16@gmail.com>
2023-07-26 16:39:40 +0530

    Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,21 @@ import Companies from './components/Companies';
 import Open_Source from './components/Open_Source';
 import Docs from './components/Docs';
 import NotFound from './components/NotFound';
-
+import ReactGA from 'react-ga4';
 
 function App() {
 
+  ReactGA.initialize([
+    {
+      trackingId: process.env.REACT_APP_GA_TRACKING_CODE,
+      gaOptions: {}, // optional
+      gtagOptions: {}, // optional
+    },
 
+  ]);
+  
+  // Send pageview with a custom path
+  ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
   return (
     <div>
         <Router>
