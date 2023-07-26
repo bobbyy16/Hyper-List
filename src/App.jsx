8dd38bb17caabb8278e0bebf5bfe858f7e8c1e2a
Abhishek k <bobbyyyyy.16@gmail.com>
2023-07-26 17:03:56 +0530

    Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,31 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home'
-import './index.css'
+import Home from './components/Home';
+import './index.css';
 import Companies from './components/Companies';
 import Open_Source from './components/Open_Source';
 import Docs from './components/Docs';
 import NotFound from './components/NotFound';
+import React, { useEffect } from 'react';
 import ReactGA from 'react-ga4';
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-K0HML6S73C");
+    ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });  }, []);
 
-  ReactGA.initialize([
-    {
-      trackingId: process.env.REACT_APP_GA_TRACKING_CODE,
-      gaOptions: {}, // optional
-      gtagOptions: {}, // optional
-    },
-
-  ]);
-  
-  // Send pageview with a custom path
-  ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
   return (
     <div>
-        <Router>
-          <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/companies" element={<Companies />}></Route>
-              <Route path='/open-source' element={<Open_Source />}></Route>
-              <Route path='/docs' element={<Docs />}></Route>
-              <Route path='*' element={<NotFound />}></Route>
-            </Routes>  
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/companies" element={<Companies />} />
+          <Route path='/open-source' element={<Open_Source />} />
+          <Route path='/docs' element={<Docs />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
       </Router>
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
